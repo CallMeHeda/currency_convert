27018f99f97b1f9cb825fe0b0ac595075dc1d170
Fatima El-Amri <fatimaelamri789@gmail.com>
2021-10-07 21:00:19 +0200

    connexion api

--- connexionApi/connexion_api.php
+++ connexionApi/connexion_api.php
@@ -8,7 +8,7 @@ function convertCurrency()
     $string = $from[0] . "_" . $to[0];
     $curl = curl_init();
     curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://free.currconv.com/api/v7/convert?q=" . $string . "&compact=ultra&apiKey=7fe003144c1aecbaad07",
+        CURLOPT_URL => "https://free.currconv.com/api/v7/convert?q=" . $string . "&compact=ultra&apiKey=MyApiKey",
         CURLOPT_RETURNTRANSFER => 1,
         CURLOPT_SSL_VERIFYPEER => false,
         CURLOPT_SSL_VERIFYHOST => false
@@ -21,5 +21,3 @@ function convertCurrency()
     $total = $rate * $amount;
     echo "$amount $from[0] = $total $to[0]";
 }
-
-?>
--- index.php
+++ index.php
@@ -1,83 +1,79 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <script src="bootstrap/jquery/jquery-3.5.1.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <script src="jquery/jquery-3.5.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
     <title>API CONNEXION</title>
 </head>
 <body>
 <?php include("connexionApi/connexion_api.php"); ?>
+<?php
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://free.currconv.com/api/v7/currencies?apiKey=MyApiKey",
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_SSL_VERIFYHOST => false
+));
+$response = curl_exec($curl);
+$response = json_decode($response, true);
+?>
 <div class="container">
-    <form action="" method="POST">
-        <?php
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://free.currconv.com/api/v7/currencies?apiKey=7fe003144c1aecbaad07",
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_SSL_VERIFYHOST => false
-        ));
-        $response = curl_exec($curl);
-        $response = json_decode($response, true);
-        ?>
-        <div class="form-group">
-            <label>
-                <select name="from">
-                    <?php
-                    foreach ($response['results'] as $oneResponse) {
-                        ?>
-                        <option class="optFrom"><?php echo $oneResponse['id'] . " - " . $oneResponse['currencyName']; ?></option>
-                    <?php } ?>
-                </select>
-            </label>
-        </div>
+    <div class="row" style="margin-bottom: 0;">
+        <form method="POST" class="col s12" id="form">
+            <div class="row" style="margin-bottom: 0;">
+                <div class="input-field col s12 m6 l3">
+                    <input id="amount" type="number" name="amount" class="validate" min="1" value="1" required>
+                    <label for="icon_prefix">Amount</label>
+                </div>
 
-        <div class="form-group">
-            <label>
-                <select name="to">
-                    <?php
-                    foreach ($response['results'] as $oneResponse) {
-                        ?>
-                        <option><?php echo $oneResponse['id'] . " - " . $oneResponse['currencyName']; ?></option>
-                    <?php } ?>
-                </select>
-            </label>
-        </div>
+                <div class="input-field col s12 m6 l3">
+                    <select name="from" class="browser-default">
+                        <?php
+                        foreach ($response['results'] as $oneResponse) {
+                            ?>
+                            <option id="optFrom" <?php if ($oneResponse['id'] == 'EUR') {
+                                echo ' selected';
+                            } ?>><?php echo $oneResponse['id'] . " - " . $oneResponse['currencyName']; ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
 
-        <div class="form-group">
-                        <label>Amount
-                            <input class="form-control" name="amount" type="text" id="amount">
-                        </label>
-        </div>
+                <i class="material-icons prefix hiddendiv">swap_horiz</i>
 
-        <input type="submit" name="sub" class="btn btn-danger" value="Convertir">
-    </form>
+                <div class="input-field col s12 m6 l3">
+                    <select name="to" class="browser-default">
+                        <?php
+                        foreach ($response['results'] as $oneResponse) {
+                            ?>
+                            <option id="optTo" <?php if ($oneResponse['id'] == 'USD') {
+                                echo ' selected';
+                            } ?>><?php echo $oneResponse['id'] . " - " . $oneResponse['currencyName']; ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+
+                <div class="input-field col s12 m6 l3">
+                    <button class="btn blue darken-1 waves-effect waves-light" type="submit" name="action">Convert</button>
+                </div>
+
+            </div>
+        </form>
+    </div>
+
     <?php
     if (isset($_POST['from']) && isset($_POST['to']) && isset($_POST['amount'])) {
         $from = $_POST["from"];
         $to = $_POST["to"];
         $amount = $_POST["amount"];
-
-        convertCurrency();
-    }
-    ?>
+       ?>
+        <div><?php convertCurrency(); ?></div>
+    <?php } ?>
 </div>
-
-<!--<script>-->
-<!--    $(document).ready(function () {-->
-<!--        $('form').on('submit', function () {-->
-<!--            $.ajax({-->
-<!--                method: 'POST',-->
-<!--                url: 'connexion_api.php',-->
-<!--                data: $('form').serialize(),-->
-<!--                success: function () {-->
-<!--                }-->
-<!--            });-->
-<!--        });-->
-<!--    })-->
-<!--</script>-->
-
 </body>
 </html>
